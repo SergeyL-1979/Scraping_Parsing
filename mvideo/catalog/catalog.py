fbf83b492247fa923eb09145e0840e4654dbbc4d
sergd <videomc1979@gmail.com>
2023-07-04 00:04:44 +0300

    refactoring

--- mvideo/catalog/catalog.py
+++ mvideo/catalog/catalog.py
@@ -19,33 +19,34 @@ def get_catalog():
 
 
 def read_catalog_save_url_and_name():
-    # with open('data/1_catalog_ids.json', encoding='utf-8') as file:
-    #     catalog_ids = json.load(file)
-    #
-    # link_url = []
-    # for cat in catalog_ids:
-    #     cat['categories'] = cat.get('name')
-    #     cat['categories'] = cat.get('url')
-    #     link_url.append({'name': cat['name'], 'url': 'https://www.mvideo.ru' + cat['url']})
-    #
-    # with open('data/2_categories_ids.json', 'w', encoding='utf-8') as file:
-    #     json.dump(link_url, file, indent=4, ensure_ascii=False)
 
     with open('data/1_catalog_ids.json', encoding='utf-8') as file:
         url_link_product = json.load(file)
 
-    link_url = []
-    for cat in url_link_product:
-        cat = cat.get('categories')
-        link_url.append(cat)
-
     with open('data/2_name_and_url_product.json', 'w', encoding='utf-8') as file:
-        json.dump(link_url, file, indent=4, ensure_ascii=False)
+        json.dump([cat for cat in url_link_product], file, indent=4, ensure_ascii=False)
+
+
+# def read_catalog_product():
+#     with open('data/2_name_and_url_product.json', encoding='utf-8') as file:
+#         product_catalog = json.load(file)
+#
+#     link_url_list = []
+#     for cat in product_catalog:
+#         cat = cat.get('categories')
+#         link_url_list.append(cat)
+#     print(link_url_list)
+#
+#     # with open('data/2_name_and_url_product.json', encoding='utf-8') as file:
+#     #     json.dump(link_url, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    get_catalog()
+    # get_catalog()
     read_catalog_save_url_and_name()
+    # read_catalog_product()
+
+
 
 if __name__ == '__main__':
     main()
