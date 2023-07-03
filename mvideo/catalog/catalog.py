7d0e6c2db333bf7a42822577d864e48f863540ca
sergd <videomc1979@gmail.com>
2023-07-03 15:47:32 +0300

    refactoring

--- mvideo/catalog/catalog.py
+++ mvideo/catalog/catalog.py
@@ -14,26 +14,24 @@ def get_catalog():
 
     catalog_ids = response.get('body').get('categories')
 
-    with open('data/1_categories_ids.json', 'w', encoding='utf-8') as file:
+    with open('data/1_catalog_ids.json', 'w', encoding='utf-8') as file:
         json.dump(catalog_ids, file, indent=4, ensure_ascii=False)
 
 
-def read_catalog_save_url_name():
-    with open('data/1_categories_ids.json', encoding='utf-8') as file:
-        catalog_ids = json.load(file)
+def read_catalog_save_url_and_name():
+    # with open('data/1_catalog_ids.json', encoding='utf-8') as file:
+    #     catalog_ids = json.load(file)
+    #
+    # link_url = []
+    # for cat in catalog_ids:
+    #     cat['categories'] = cat.get('name')
+    #     cat['categories'] = cat.get('url')
+    #     link_url.append({'name': cat['name'], 'url': 'https://www.mvideo.ru' + cat['url']})
+    #
+    # with open('data/2_categories_ids.json', 'w', encoding='utf-8') as file:
+    #     json.dump(link_url, file, indent=4, ensure_ascii=False)
 
-    link_url = []
-    for cat in catalog_ids:
-        cat['categories'] = cat.get('name')
-        cat['categories'] = cat.get('url')
-        link_url.append({'name': cat['name'], 'url': 'https://www.mvideo.ru' + cat['url']})
-
-    with open('data/2_categories_ids.json', 'w', encoding='utf-8') as file:
-        json.dump(link_url, file, indent=4, ensure_ascii=False)
-
-
-def reader_url_product():
-    with open('data/1_categories_ids.json', encoding='utf-8') as file:
+    with open('data/1_catalog_ids.json', encoding='utf-8') as file:
         url_link_product = json.load(file)
 
     link_url = []
@@ -41,15 +39,13 @@ def reader_url_product():
         cat = cat.get('categories')
         link_url.append(cat)
 
-    with open('data/3_categories_ids.json', 'w', encoding='utf-8') as file:
+    with open('data/2_name_and_url_product.json', 'w', encoding='utf-8') as file:
         json.dump(link_url, file, indent=4, ensure_ascii=False)
 
 
 def main():
     get_catalog()
-    read_catalog_save_url_name()
-    reader_url_product()
-
+    read_catalog_save_url_and_name()
 
 if __name__ == '__main__':
     main()
